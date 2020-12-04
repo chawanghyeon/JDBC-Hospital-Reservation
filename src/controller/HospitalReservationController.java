e9197a6304d08bd8370910c652f88961af0c210f
kowo1001 <kowo1001@dongguk.edu>
2020-12-04 11:14:39 +0900

    complete Endview

--- src/controller/HospitalReservationController.java
+++ src/controller/HospitalReservationController.java
@@ -1,7 +1,17 @@
 package controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import model.CustodianDAO;
+import model.InformationRecipientDAO;
+import model.PatientDAO;
+import model.ProblemDAO;
+import model.domain.CustodianDTO;
+import model.domain.InformationRecipientDTO;
 import model.domain.PatientDTO;
 import model.domain.ProblemDTO;
+import view.EndView;
 
 public class HospitalReservationController {
 	private static HospitalReservationController instance = new HospitalReservationController();
@@ -12,28 +22,60 @@ public class HospitalReservationController {
 	public static HospitalReservationController getInstance() {
 		return instance;
 	}
-
-	public void getPatient(String patientId) {
-
+	
+	// 환자 Id로 환자 검색 ,환자 Id로 예약 정보 확인
+	public static PatientDTO getPatient(String patientId) {
+		PatientDTO patient = null;
+		try {
+			patient = PatientDAO.getPatient(patientId);
+		} catch(SQLException e){
+			e.printStackTrace();
+			EndView.showError("환자 id로 해당 환자 검색 오류");
+		}
+		return patient;
 	}
-
-	public void getAllInfromationRecipient() {
-
+	
+	// 모든 의료기관 검색
+	public static ArrayList<InformationRecipientDTO> getAllInfromationRecipient() {
+		ArrayList<InformationRecipientDTO> allInformationRecipient = null;
+		try{
+			allInformationRecipient = InformationRecipientDAO.getAllInformationRecipients();
+		}catch(SQLException s) {
+			s.printStackTrace();
+			EndView.showError("모든 의료기관 검색시 에러 발생");
+		}
+		return allInformationRecipient;
 	}
-
-	public void getAllCustodian() {
-
+	
+	// 모든 의사 검색
+	public static ArrayList<CustodianDTO> getAllCustodian() {
+		ArrayList<CustodianDTO> allCustodian = null;
+		try{
+			allCustodian = CustodianDAO.getAllCustodians();
+		}catch(SQLException s) {
+			s.printStackTrace();
+			EndView.showError("모든 의사 검색시 에러 발생");
+		}
+		return allCustodian;
 	}
-
+	
+	// 특정 의사 선택해서 예약
 	public void addPatient(PatientDTO patient) {
 
 	}
-
+	
+	// 진료 완료 후 진단 내역 생성
 	public void addProblem(ProblemDTO problem) {
 
 	}
-
-	public void getProblem(String patientId) {
-
+	// 진단 내역 출력 후 다음 단계 출력
+	public static ProblemDTO getProblem(String patientId) {
+		ProblemDTO problem = null;
+		try {
+			problem = ProblemDAO.getProblem(problemcode)
+		}catch() {
+			
+		}
 	}
+
 }
--- src/view/EndView.java
+++ src/view/EndView.java
@@ -1,5 +1,50 @@
 package view;
 
+import java.util.ArrayList;
+
+import model.domain.PatientDTO;
+import model.domain.ProblemDTO;
+
 public class EndView {
 
+	// 환자 id로 환자 검색
+	public static void patientView(PatientDTO patient) {
+		System.out.println(patient);
+	}
+		
+	// 모든 의료기관 검색
+	public static void informationRecipientListView(ArrayList allInformationRecipient) {
+		int length = allInformationRecipient.size();
+		
+		if(length != 0) {
+			for(int index = 0; index < length; index++) {
+				System.out.println("의료기관 정보 검색" + (index+1) + "-" + allInformationRecipient.get(index));
+			}
+		}else {
+			System.out.println("검색된 정보가 존재하지 않습니다.");
+		}
+	}
+
+	// 모든 의사 검색
+	public static void custodianListView(ArrayList allCustodian) {
+		int length = allCustodian.size();
+		
+		if(length != 0) {
+			for(int index = 0; index < length; index++) {
+				System.out.println("의료진 정보 검색" + (index+1) + "-" + allCustodian.get(index));
+			}
+		}else {
+			System.out.println("검색된 정보가 존재하지 않습니다.");
+		}
+	}
+	
+	// 진단 내역 출력 후 다음 단계 출력
+	public static void problemView(ProblemDTO problem) {
+		System.out.println(problem);
+	}
+		
+	// 예외 상황 출력
+	public static void showError(String message) {
+		System.out.println(message);
+	}
 }
